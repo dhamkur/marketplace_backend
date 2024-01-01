93de2dd6e03fa6c2ca8e486b937273388c338029
Adham Kurniawan <adhamkurniawan29@gmail.com>
2024-01-01 10:21:59 +0700

    implemented alert auto disappear

---
+++ app/javascript/controllers/alert_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["source"];
+
+  initialize() {
+    let alerts = document.querySelectorAll(".alert");
+
+    alerts.forEach((alert) => {
+      setTimeout(() => {
+        alert.classList.remove("show");
+      }, 3000);
+    });
+  }
+}

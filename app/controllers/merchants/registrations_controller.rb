f0edbab104de50360631adedff67b9f553c3c22c
Adham Kurniawan <adhamkurniawan29@gmail.com>
2024-03-12 19:47:27 +0700

    remove unnecesary merchants views from devise, implement bootstrap styled sessions and registrations merchants

--- app/controllers/merchants/registrations_controller.rb
+++ app/controllers/merchants/registrations_controller.rb
@@ -1,2 +1,12 @@
 class Merchants::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters
+
+  protected
+
+  def configure_permitted_parameters
+    keys = [:pic_name, :pic_phone_number, :merchant_name]
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: keys)
+  end
 end
